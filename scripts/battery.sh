4face865e548d3791275d0acaeb15d19746f8daa
Angell6991 <miguel9196.guzman@gmail.com>
2025-04-04 00:34:43 -0500

    not_charging

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -9,6 +9,7 @@ battery=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
 ctl_batt_full="/tmp/battery_full"
+ctl_batt_not="/tmp/battery_not"
 ctl_batt_dis="/tmp/battery_dischar"
 
 
@@ -17,9 +18,16 @@ if      [ "$status" = "Full" ]  &&  [ ! -f "$ctl_batt_full" ]; then
     ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
     touch   $ctl_batt_full
 
+
+elif    [ "$status" = "Not charging" ]  &&  [ ! -f "$ctl_batt_not" ]; then
+    notify-send "󰂃 Battery: $battery%" "Not charging"
+    ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
+    touch   $ctl_batt_not
+
+
 elif    [ "$status" = "Discharging" ]; then
     
-    if  [ $battery -le 15 ]  &&  [ ! -f "$ctl_batt_dis" ]; then
+    if  [ $battery -le 20 ]  &&  [ ! -f "$ctl_batt_dis" ]; then
         notify-send "󰁻 Battery: $battery%" "low battery level"
         ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
         touch   $ctl_batt_dis
@@ -29,6 +37,7 @@ elif    [ "$status" = "Discharging" ]; then
 elif    [ "$status" = "Charging" ]; then
     rm -f   $ctl_batt_full
     rm -f   $ctl_batt_dis    
+    rm -f   $ctl_batt_not    
 
 
 fi
