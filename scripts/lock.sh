fc8dc6fb641b987caffa6d63b2af655a8700a715
Angell6991 <email----@gmail.com>
2025-06-12 18:09:33 -0500

    config_suspend_system

---
+++ scripts/lock.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Verificamos si hypridle está corriendo
+
+if pgrep -x "hypridle" > /dev/null; then
+    notify-send "󱚳  System security:" "automatic screen lock disabled"
+    ~/.eww/target/release/eww -c ~/.config/eww/widget_lock open lock-icon-glass &
+    pkill -x hypridle
+
+else
+    notify-send "󱎚  System security:" "automatic screen lock activated"
+    ~/.eww/target/release/eww -c ~/.config/eww/widget_lock kill &
+    hypridle &
+
+fi
