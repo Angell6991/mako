9f21cbd1f2d87c4d9f6aec94da286aa748f5657b
Angell6991 <miguel9196.guzman@gmail.com>
2026-03-01 18:30:28 -0500

    add starcraft

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -4,6 +4,9 @@ export  DISPLAY=:0
 export  DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 export  PIPEWIRE_REMOTE=/run/user/1000/pipewire-0  
 
+init_adjutora=$(~/.eww/target/release/eww -c ~/.config/mako/scripts/widget_starcarft  open adjutora-icon-glass)
+kill_adjutora=$(~/.eww/target/release/eww -c ~/.config/mako/scripts/widget_starcarft  kil)
+
 
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
@@ -14,26 +17,32 @@ ctl_batt_dis="/tmp/battery_dischar"
 
 
 if      [ "$status" = "Full" ]  &&  [ ! -f "$ctl_batt_full" ]; then
+    init_adjutora
     notify-send "󱈑 Battery: $battery%" "full charge"
     # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
-    ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/starcratf/investigacion.mp3
+    ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/investigacion.mp3
     touch   $ctl_batt_full
+    sleep 2
+    kill_adjutora
 
 
 elif    [ "$status" = "Not charging" ]  &&  [ ! -f "$ctl_batt_not" ]; then
     notify-send "󰂃 Battery: $battery%" "Not charging"
     # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
-    ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/starcratf/aterrizaje.mp3
+    ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/aterrizaje.mp3
     touch   $ctl_batt_not
-
+    sleep 2
+    kill_adjutora
 
 elif    [ "$status" = "Discharging" ]; then
     
     if  [ $battery -le 20 ]  &&  [ ! -f "$ctl_batt_dis" ]; then
         notify-send "󰁻 Battery: $battery%" "low battery level"
         # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
-        ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/starcratf/energia.mp3
+        ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/energia.mp3
         touch   $ctl_batt_dis
+        sleep 2
+        kill_adjutora
     fi
 
 
