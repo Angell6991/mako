7d226856998ca33faf2b27ddf3df32264656474e
Angell6991 <miguel9196.guzman@gmail.com>
2026-03-01 21:36:50 -0500

    add starcraft

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -4,10 +4,6 @@ export  DISPLAY=:0
 export  DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 export  PIPEWIRE_REMOTE=/run/user/1000/pipewire-0  
 
-init_adjutora=$(~/.eww/target/release/eww -c ~/.config/mako/scripts/widget_starcarft  open adjutora-icon-glass)
-kill_adjutora=$(~/.eww/target/release/eww -c ~/.config/mako/scripts/widget_starcarft  kil)
-
-
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
 
@@ -17,22 +13,17 @@ ctl_batt_dis="/tmp/battery_dischar"
 
 
 if      [ "$status" = "Full" ]  &&  [ ! -f "$ctl_batt_full" ]; then
-    init_adjutora
     notify-send "󱈑 Battery: $battery%" "full charge"
     # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
     ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/investigacion.mp3
     touch   $ctl_batt_full
-    sleep 2
-    kill_adjutora
 
 
-elif    [ "$status" = "Not charging" ]  &&  [ ! -f "$ctl_batt_not" ]; then
+elif    [ "$status" = "Not charging" ]  &&  [ ! -f "$ctl_batt_not" ]; then 
     notify-send "󰂃 Battery: $battery%" "Not charging"
     # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
     ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/aterrizaje.mp3
     touch   $ctl_batt_not
-    sleep 2
-    kill_adjutora
 
 elif    [ "$status" = "Discharging" ]; then
     
@@ -41,8 +32,7 @@ elif    [ "$status" = "Discharging" ]; then
         # ffplay -nodisp -autoexit ~/.config/mako/scripts/songs/Blip.mp4
         ffplay -nodisp -autoexit ~/.config/mako/scripts/widget_starcarft/songs/energia.mp3
         touch   $ctl_batt_dis
-        sleep 2
-        kill_adjutora
+
     fi
 
 
