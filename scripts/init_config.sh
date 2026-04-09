dedc09e287e9795b7326e246ab7471d527aa0237
Angell6991 <miguel9196.guzman@gmail.com>
2026-04-09 14:35:11 -0500

    colors

--- scripts/init_config.sh
+++ scripts/init_config.sh
@@ -2,12 +2,15 @@
 
 WM=$(echo $XDG_SESSION_DESKTOP)
 CONFIGDIR="$HOME/.config/fuzzel"
+NOTIFY="$HOME/.config/mako"
 
 if [[ "$WM" == "Hyprland" ]]; then
   cp "$CONFIGDIR/fuzzel-hyprland.ini" "$CONFIGDIR/fuzzel.ini"
+  cp "$NOTIFY/config-hyprland" "$NOTIFY/config"
 
 elif [[ "$WM" == "niri" ]]; then
   cp "$CONFIGDIR/fuzzel-niri.ini" "$CONFIGDIR/fuzzel.ini"
+  cp "$NOTIFY/config-niri" "$NOTIFY/config"
 
 fi
 
